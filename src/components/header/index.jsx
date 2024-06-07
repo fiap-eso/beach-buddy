89742ad591e5a20e73bf75af41fd3a068040d714
Matheus Alvarenga <matheus.alvarenga0302@gmail.com>
2024-06-07 19:16:41 -0300

    feat: add header links

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,15 +1,22 @@
+import { useNavigate } from 'react-router-dom';
 import logo from '../../assets/logo_h_color.svg';
 
 import * as S from './styles';
 
 export function Header({ active }) {
+  const navigate = useNavigate();
+
   return (
     <S.Wrapper>
       <S.Logo src={logo} />
 
       <S.Nav>
-        <S.Link active={active === 0}>Praias</S.Link>
-        <S.Link active={active === 1}>Experiências</S.Link>
+        <S.Link onClick={() => navigate('/')} active={active === 0}>
+          Praias
+        </S.Link>
+        <S.Link onClick={() => navigate('/experiences')} active={active === 1}>
+          Experiências
+        </S.Link>
       </S.Nav>
 
       <S.Side>
